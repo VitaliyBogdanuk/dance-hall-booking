7078cd9fbbb77aa38c723b0c3b5b38769d12c73d
Vitalii Bohdaniuk <v.bogdanuk@gmail.com>
2026-01-19 23:52:53 +0200

    Delete src directory

--- src/server/validation/bookings.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import { objectId } from "./common";
-
-export const createBookingBody = z.object({
-  classSessionId: objectId,
-  childId: objectId,
-});
-
-export const bookingIdParams = z.object({ id: objectId });
